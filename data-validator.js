c5cda41d0c3935fe5c25260a3547c9ac99c11fef
Hiroki Honda <cside.story@gmail.com>
2012-04-03 15:39:36 +0900

    dont use external module

--- data-validator.js
+++ data-validator.js
@@ -1,7 +1,21 @@
 (function () {
 
 Object.prototype.each   = function(c){var o=this;var r=[];var i=0;for(var k in o) if(o.hasOwnProperty(k)){r.push(c.apply(o,[k,o[k],i++]))}return r}; 
-var sprintf = require('./sprintf');
+var sprintf = function (str) {
+    var args = Array.prototype.slice.call(arguments, 1);
+    return str.replace(/%0(\d+)d/g, function(m, num) {
+        var r = String(args.shift());
+        var c = '';
+        num = parseInt(num) - r.length;
+        while (--num >= 0) c += '0';
+        return c + r;
+    }).replace(/%[sdf]/g, function(m) { return sprintf._SPRINTF_HASH[m](args.shift()) });
+};
+sprintf._SPRINTF_HASH = {
+    '%s': String,
+    '%d': parseInt,
+    '%f': parseFloat
+};
 
 var TYPE_MAP = {
     'isString'  :function(obj) { return toString.call(obj) == '[object String]'   },
